5dd3eb6b8318faa98984bda12c69af1a5f878808
TechLionDev <jr.isamuel08.dev@gmail.com>
2022-11-28 22:13:03 -0500

    Enhance

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 class Main {
     public static void main(String[] args) {
+
+    }
+}
+
+class genericPizzas {
+    public static void main(String[] args) {
         System.out.println("\n");
         String[] smallToppings = {"Peperoni", "Olives"};
         String[] mediumToppings = {"Peperoni", "Olives", "Peppers", "Anchovies"};
         String[] largeToppings = {"Peperoni", "Olives", "Peppers", "Anchovies", "Pineapple", "Tuna", "Chicken"};
-        Pizza mySmallPizza = new Pizza(PizzaSize.SMALL, smallToppings);
+        Pizza mySmallPizza = new Pizza(PizzaSize.SMALL, smallToppings, 5);
         System.out.println(mySmallPizza.getDescription());
-        Pizza myMediumPizza = new Pizza(PizzaSize.MEDIUM, mediumToppings);
+        Pizza myMediumPizza = new Pizza(PizzaSize.MEDIUM, mediumToppings, 1);
         System.out.println(myMediumPizza.getDescription());
-        Pizza myLargePizza = new Pizza(PizzaSize.LARGE, largeToppings);
+        Pizza myLargePizza = new Pizza(PizzaSize.LARGE, largeToppings, 6);
         System.out.println(myLargePizza.getDescription());
-
     }
 }
